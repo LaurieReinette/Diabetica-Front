608ad122e0a5984a73cafaea6b34e0bfdaeaf581
LaurieReinette <laurie97275@hotmail.fr>
2020-11-14 11:45:07 +0100

    initialisation of app, components and main structure

---
+++ src/components/Diabie/index.js
@@ -0,0 +1,18 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './styles.scss';
+
+// == Composant
+const Diabie = () => (
+  <div className="diabie">
+    <div>Header</div>
+    <div>Main</div>
+    <div>Subscribe</div>
+    <div>Footer</div>
+  </div>
+);
+
+// == Export
+export default Diabie;
